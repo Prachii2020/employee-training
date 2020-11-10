798f4ac85cdda97f5c15a301fe682fa0b5aa4e95
hunaidhanfee20 <64194049+hunaidhanfee20@users.noreply.github.com>
2020-11-10 19:33:07 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.EmployeeTraining.Test/Helpers/IUserEventSearchServic.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="UserEventsTest.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.EmployeeTraining.Tests.Helpers
+{
+    internal interface IUserEventSearchServic
+    {
+    }
+}
